38d823b795d1b47a159b946bb3f31d311399fa17
clementGilardy <gilardy.clement@gmail.com>
2018-06-15 21:33:01 +0200

    correction de la base de données

--- server/database/database.js
+++ server/database/database.js
@@ -11,69 +11,124 @@ module.exports = {
 };
 
 function find(docName, filter) {
-	const deffered = Q.defer();
+	const deffered          = Q.defer();
+	let currentDbConnection = null;
 	getMongoDB()
 		.then((dbConnection) => {
-			dbConnection.collection(docName).find(filter).toArray(function (err, result) {
+			currentDbConnection = dbConnection;
+			const defferedForDb = Q.defer();
+			dbConnection.db('manon').collection(docName).find(filter).toArray(function (err, result) {
 				if (err) {
-					deffered.reject(err);
+					defferedForDb.reject(err);
 				}
 				else {
-					deffered.resolve(result);
+					defferedForDb.resolve(result);
 				}
 			});
-		});
+			return defferedForDb.promise;
+		}).then((result) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.resolve(result);
+	}).catch((error) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.reject(error);
+	});
 	
 	return deffered.promise;
 }
 
 function findLimit(docName, filter, limit) {
-	const deffered = Q.defer();
+	const deffered          = Q.defer();
+	let currentDbConnection = null;
 	getMongoDB()
 		.then((dbConnection) => {
-			dbConnection.collection(docName)
+			currentDbConnection = dbConnection;
+			const defferedForDb = Q.defer();
+			dbConnection.db('manon').collection(docName)
 				.find(filter)
 				.limit(+limit)
 				.toArray(function (err, result) {
 					if (err) {
-						deffered.reject(err);
+						defferedForDb.reject(err);
 					}
 					else {
-						deffered.resolve(result);
+						defferedForDb.resolve(result);
 					}
 				});
-		});
+			return defferedForDb.promise;
+		}).then((result) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.resolve(result);
+	}).catch((error) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.reject(error);
+	});
 	
 	return deffered.promise;
 }
 
 function del(docName, data) {
-	const deffered = Q.defer();
+	const deffered          = Q.defer();
+	let currentDbConnection = null;
 	getMongoDB()
 		.then((dbConnection) => {
-			dbConnection.collection(docName).remove(data, {justOne: true}, function (err, result) {
+			const defferedForDb = Q.defer();
+			currentDbConnection = dbConnection;
+			dbConnection.db('manon').collection(docName).remove(data, {justOne: true}, function (err, result) {
 				if (err)
-					deffered.reject(err);
+					defferedForDb.reject(err);
 				else
-					deffered.resolve(result);
+					defferedForDb.resolve(result);
 			});
-		});
+			return defferedForDb.promise;
+		}).then((result) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.resolve(result);
+	}).catch((error) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.reject(error);
+	});
 	
 	return deffered.promise;
 }
 
 function save(docName, dataInsert) {
-	const deffered = Q.defer();
+	const deffered          = Q.defer();
+	let currentDbConnection = null;
 	getMongoDB()
 		.then((dbConnection) => {
-			dbConnection.collection(docName).save(dataInsert, function (err, result) {
+			const defferedForDb = Q.defer();
+			currentDbConnection = dbConnection;
+			dbConnection.db('manon').collection(docName).save(dataInsert, function (err, result) {
 				if (err)
-					deffered.reject(err);
+					defferedForDb.reject(err);
 				else
-					deffered.resolve(result.ops[0]);
+					defferedForDb.resolve(result.ops[0]);
 			});
-		});
-	
+			return defferedForDb.promise;
+		}).then((result) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.resolve(result)
+	}).catch((error) => {
+		if (currentDbConnection) {
+			currentDbConnection.close();
+		}
+		deffered.reject(error);
+	});
 	return deffered.promise;
 }
 
@@ -89,10 +144,9 @@ function getMongoDB() {
 }
 
 function connectMongoDB(callback) {
-	MongoClient.connect('mongodb://hawklm:Vetbopen_36@ds159459.mlab.com:59459/manon', function (err, dbb) {
-	// MongoClient.connect('mongodb://localhost:27017/manon', function (err, dbb) {
+	// MongoClient.connect('mongodb://hawklm:Vetbopen_36@ds159459.mlab.com:59459/manon', function (err, dbb) {
+	MongoClient.connect('mongodb://localhost:27017/manon', function (err, dbb) {
 		if (err) return console.log(err);
-		const db = dbb.db('manon');
-		callback(err, db);
+		callback(err, dbb);
 	});
 }
