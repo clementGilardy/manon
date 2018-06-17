35025b1be5561ef9b0534a8a7af2e22cbbe14fa1
clementGilardy <gilardy.clement@gmail.com>
2018-06-17 14:14:12 +0200

    correction de bug au niveau du header

--- src/app/projects/nav-project/navproject.component.ts
+++ src/app/projects/nav-project/navproject.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+
+declare var $: any;
 
 @Component({
 	           selector   : 'app-navproject',
 	           templateUrl: 'navproject.component.html',
 	           styleUrls  : ['navproject.component.scss']
            })
-export class NavprojectComponent {
-	constructor() {
+export class NavprojectComponent implements OnInit {
+
+	@ViewChild('nav') navigation: ElementRef;
+	private lastScrolledTop: number;
+	private delta: number;
+
+	constructor(@Inject(DOCUMENT) private document: Document) {
+		this.lastScrolledTop = 0;
+		this.delta           = 5;
 	}
+
+	ngOnInit() {
+		$(".right.button-collapse").sideNav();
+	}
+
+	/**
+	 * Controle du scroll
+	 */
+	@HostListener("window:scroll", [])
+	onWindowScroll() {
+		const offset = this.document.documentElement.scrollTop;
+		if (offset >= 0 && offset <= 100) {
+			$('#' + this.navigation.nativeElement.id).css('background', 'none');
+			$('#' + this.navigation.nativeElement.id + ' a').css('color', 'white');
+		} else {
+			$('#' + this.navigation.nativeElement.id).css('background', 'white');
+			$('#' + this.navigation.nativeElement.id + ' a').css('color', 'black');
+		}
+
+		if (offset > this.lastScrolledTop && offset > 80) {
+			$('#' + this.navigation.nativeElement.id).fadeOut(500);
+		} else {
+			$('#' + this.navigation.nativeElement.id).fadeIn(500);
+		}
+
+		this.lastScrolledTop = offset;
+	}
+
 }
