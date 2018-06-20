8685d9fc640b9f63f6cedc3f6d088cce1bd5f437
clementGilardy <gilardy.clement@gmail.com>
2018-06-20 18:14:43 +0200

    correction mineur de bug graphique sur mobile

--- src/common/commons.module.ts
+++ src/common/commons.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from "common/components/footer/footer.component";
-import { NavComponent } from "common/components/nav/nav.component";
 import { ProjectService } from "common/services/project.service";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
-	          exports     : [FooterComponent, NavComponent],
+	          exports     : [FooterComponent],
 	          imports     : [RouterModule],
 	          declarations: [
-		          FooterComponent,
-		          NavComponent
+		          FooterComponent
 	          ],
 	          providers   : [ProjectService],
 	          bootstrap   : []
